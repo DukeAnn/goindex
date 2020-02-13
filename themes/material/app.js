495d7ea15ae2898933793625a27e5edbdc1a12c9
andakai <andakai@jd.com>
2020-02-13 17:32:06 +0800

    测试

--- themes/material/app.js
+++ themes/material/app.js
@@ -1,5 +1,8 @@
 // 在head 中 加载 必要静态
 document.write('<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/mdui@0.4.3/dist/css/mdui.min.css">');
+// Dplayer
+document.write('<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/dplayer/dist/DPlayer.min.css">');
+document.write('<script src="//cdn.jsdelivr.net/npm/dplayer/dist/DPlayer.min.js"></script>');
 // markdown支持
 document.write('<script src="//cdn.jsdelivr.net/npm/markdown-it@10.0.0/dist/markdown-it.min.js"></script>');
 document.write('<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>*{padding:0 6px;margin:0 2px}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>.mdui-typo-headline{padding:0 1pc 0 0}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}.mdui-container{width:100%!important;margin:0}.mdui-toolbar>.mdui-typo-headline,.mdui-toolbar>a:last-child,.mdui-toolbar>i:first-child{display:block}}</style>');
@@ -17,6 +20,13 @@ function init(){
 	`;
     $('body').html(html);
 }
+
+const dp = new DPlayer({
+    container: document.getElementById('dplayer'),
+    video: {
+        url: '',
+    },
+});
 
 function render(path){
 	if(path.indexOf("?") > 0){
@@ -255,9 +265,11 @@ function file_code(path){
 // 文件展示 视频 |mp4|webm|avi|
 function file_video(path){
 	var url = window.location.origin + path;
+	dp.video.url=url
 	var content = `
 <div class="mdui-container-fluid">
 	<br>
+	<div id="dplayer"></div>
 	<video class="mdui-video-fluid mdui-center" preload controls>
 	  <source src="${url}" type="video/mp4">
 	</video>
